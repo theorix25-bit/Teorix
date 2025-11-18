302d285ec2e3eaa109202e8454d8f3192470a5fc
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2025-11-18 15:22:27 -0300

    ultima prueba del webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,34 +6,33 @@ import { updatePlanUser } from "@/hooks/useSupabase";
 
 export async function POST(req: Request) {
   const stripe = createClienteStripe();
-  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+
   const body = await req.text();
   const headersList = await headers();
   const sig = headersList.get("stripe-signature")!;
-  console.log("webhook recibido");
+  const endpointSecret = process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET!;
 
   let event: Stripe.Event;
 
   try {
+    console.log(endpointSecret);
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err: any) {
     console.error("❌ Error verificando webhook", err.message);
     return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
   }
 
-  // 👇 Evento principal: pago confirmado del Checkout
-  if (event.type === "checkout.session.completed") {
-    const session = event.data.object as Stripe.Checkout.Session;
+  switch (event.type) {
+    case "checkout.session.completed":
+      const session = event.data.object as Stripe.Checkout.Session;
+      const userId = session.metadata?.userId;
+      const subscription_id = session.metadata?.planId;
+      updatePlanUser(userId, subscription_id);
+      break;
 
-    const userId = session.metadata?.userId;
-    const subscription_id = session.metadata?.planId;
-    const subscriptionId = session.subscription;
-    console.log(subscriptionId);
-    console.log("Pago confirmado de:", userId);
-    console.log("Subscription ID:", subscriptionId);
-
-    updatePlanUser(userId, subscription_id);
+    default:
+      console.log(`Evento desconocido ${event.type}`);
   }
 
-  return NextResponse.json({ received: true });
+  return NextResponse.json(null, { status: 200 });
 }
