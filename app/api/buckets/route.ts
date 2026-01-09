af08fceff8a61ae68df525a52bcacd33f64eed52
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2026-01-09 06:58:29 -0300

    ajuste de variables

--- app/api/buckets/route.ts
+++ app/api/buckets/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 export async function GET() {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY! 
+    process.env.SUPABASE_SERVICE_ROLE_KEY! 
   );
 
   const { data, error } = await supabaseAdmin.storage.listBuckets();
--- app/clases/documentos/page.tsx
+++ app/clases/documentos/page.tsx
@@ -6,7 +6,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 // export async function getBuckets() {
