3e91a0b1befe4abf69ccd665c1eda95603477689
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2026-01-09 10:35:51 -0300

    cambios en la url del redireccionamiento a nuevo dominio

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -9,7 +9,7 @@ export function SignUpForm() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const emailRedirectTo = `${process.env.NEXT_PUBLIC_URL}clases`;
+  const emailRedirectTo = `https://teorix.es/clases`;
   const [repeatPassword, setRepeatPassword] = useState("");
   const [terminos, setTerminos] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
